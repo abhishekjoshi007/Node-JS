310cffb07a59668880f698747375d8ea358303e8
Abhishek Joshi <abhishekjoshi.ddun@gmail.com>
2020-11-02 10:33:07 +0530

    Sharing data across request and users.

--- Dynamic Content and template (Chapter 5)/routes/admin.js
+++ Dynamic Content and template (Chapter 5)/routes/admin.js
@@ -6,6 +6,9 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+//New var for storing data
+const products =[];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -13,8 +16,13 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  //console.log(req.body);
+  products.push({title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+//module.exports=router;
+
+//expoting routes and products
+exports.routes=router;
+exports.products=products;

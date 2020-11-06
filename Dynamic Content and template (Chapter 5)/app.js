3d6393764fb3bb8d0462e27a7eac3cec114e05dc
Abhishek Joshi <abhishekjoshi.ddun@gmail.com>
2020-11-06 14:36:38 +0530

    Working with Handlebars.

--- Dynamic Content and template (Chapter 5)/app.js
+++ Dynamic Content and template (Chapter 5)/app.js
@@ -2,13 +2,22 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const expressHbs=require('express-handlebars');
 const app = express();
 
+//registering new template engine (Hbs)
+app.engine('hbs',expressHbs());
+
 //telling experess that i am registering template engine pug
-app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
 //view  allows us to tell express where to find these dynamic views.
 app.set('views','views')
+
+
+//telling experess that i am registering template engine pug
+// app.set('view engine', 'pug');
+//view  allows us to tell express where to find these dynamic views.
+// app.set('views','views')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');

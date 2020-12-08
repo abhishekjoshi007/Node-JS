5f40913359ce8401555be1da7a224f29e02d243c
Abhishek Joshi <abhishekjoshi.ddun@gmail.com>
2020-12-08 15:06:52 +0530

    MVC 404 Added.

--- Model View Controller (MVC) (Chapter 6)/app.js
+++ Model View Controller (MVC) (Chapter 6)/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorController =require('./controllers/error');
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);

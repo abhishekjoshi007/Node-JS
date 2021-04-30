ec2ac4ceddf753c5ac887f6625d4829f461f373e
Star World Digital <swdadmin@Stars-MacBook-Pro.local>
2021-04-30 21:09:55 +0530

    Assignment

--- Working with express.js (Chapter 4)/Assignment/app.js
+++ Working with express.js (Chapter 4)/Assignment/app.js
@@ -1,13 +1,25 @@
 const express=require('express');
 
+const bodyparser=require('body-parser') ;
+
+const path=require('path');
+
 const adminroutes=require('./routes/admin');
 
 const shoproutes=require('./routes/shop');
+
+
 
 const app=express();
 
 app.use(adminroutes);
 app.use(shoproutes);
+
+app.use((req,res,next) =>
+{
+
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
 
 
 app.listen(3000);
--- Working with express.js (Chapter 4)/Assignment/routes/admin.js
+++ Working with express.js (Chapter 4)/Assignment/routes/admin.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const path=require('path');
+
 const router=express.Router();
 
 router.get('/',(req,res,next) => 
 {
     res.sendFile(path.join(__dirname,'../','views','admin.html'));
+    
 })
 
 module.exports=router;

70225fa143e4cfb541535345a90b16579033792c
Star World Digital <abhishekjoshi.ddun@gmail.com>
2021-06-04 23:34:38 +0530

    Query params

--- Dynamic Routes & Advance Models (Chapter 8)/controllers/admin.js
+++ Dynamic Routes & Advance Models (Chapter 8)/controllers/admin.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product');
 
+//For Adding A Product
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true
   });
 };
 
@@ -18,6 +19,26 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+//For Editing A Product
+exports.getEditProduct = (req, res, next) => {
+  
+  const editmode= req.query.edit;
+
+  if(!editmode)
+  {
+    res.redirect('/');
+  }
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    //to check what to do edit or add 
+    editing: editmode
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true
+  });
 };
 
 exports.getProducts = (req, res, next) => {

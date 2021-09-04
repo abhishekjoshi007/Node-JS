e88abe2a967b36df452b8ba9cfb40e9cdea1d44e
Star World Digital <abhishekjoshi.ddun@gmail.com>
2021-09-04 23:01:56 +0530

    Fetching Products

--- SQL_Intro (Chapter 9)/app.js
+++ SQL_Intro (Chapter 9)/app.js
@@ -15,13 +15,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 //excute help in execute query
-db.execute('SELECT * FROM products')
-  .then(result => {
-    console.log(result[0], result[1]);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// db.execute('SELECT * FROM products')
+//   .then(result => {
+//     console.log(result[0], result[1]);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));

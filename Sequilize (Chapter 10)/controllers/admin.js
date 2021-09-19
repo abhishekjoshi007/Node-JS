e1c6dcf99fa76b697fd95daf65699efb1d9b3e27
Star World Digital <abhishekjoshi.ddun@gmail.com>
2021-09-19 21:38:03 +0530

    Deleting products

--- Sequilize (Chapter 10)/controllers/admin.js
+++ Sequilize (Chapter 10)/controllers/admin.js
@@ -32,6 +32,7 @@ exports.postAddProduct = (req, res, next) => {
   }).then(result => {
     //console.log(result);
     console.log('Product Created');
+    res.redirect('/admin/products');
   } ).catch(err => {
     console.log(err);
   })
@@ -103,6 +104,17 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  //Product.deleteById(prodId);
+  //we dont have delete by id in sequelize
+  // we have destroy in sequelize
+ Product.findByPk(prodId)
+ .then(product => {
+   return product.destroy();
+ })
+ .then (result => {
+   console.log("product destroyed");
+   res.redirect('/admin/products');
+ })
+ .catch(err => console.log(err));
+
 };
